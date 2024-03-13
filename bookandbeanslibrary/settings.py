ad22cd02f37609434c4bc8b8f67f0540cc592b67
samiya1859 <samiya1859@gmail.com>
2024-03-14 00:20:45 +0600

    csrf token

--- bookandbeanslibrary/settings.py
+++ bookandbeanslibrary/settings.py
@@ -32,9 +32,9 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
-CSRF_TRUSTED_ORIGINS = [
-    '', 'https://*.127.0.0.1'
-    ]
+# CSRF_TRUSTED_ORIGINS = [
+#     '', 'https://*.127.0.0.1'
+#     ]
 # Application definition
 
 INSTALLED_APPS = [
